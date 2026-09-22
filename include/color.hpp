0d37ff74a44a29304ee224d0bec18fbfc423156b
Renan-G-projec <renanfartes@gmail.com>
2026-09-21 22:35:13 -0300

    feat: Basic color constants added. AMDG

--- include/color.hpp
+++ include/color.hpp
@@ -7,4 +7,13 @@ struct Color {
     std::uint8_t r, g, b;
 };
 
+constexpr Color WHITE{255, 255, 255};
+constexpr Color BLACK{0, 0, 0};
+constexpr Color RED{255, 0, 0};
+constexpr Color GREEN{0, 255, 0};
+constexpr Color BLUE{0, 0, 255};
+constexpr Color YELLOW{255, 255, 0};
+constexpr Color CYAN{0, 255, 255};
+constexpr Color PURPLE{255, 0, 0};
+
 #endif
